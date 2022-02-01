31461bb069f7773940b6e751f6480acf4e10b6f1
Vivien Leclercq <67290929+vilecler@users.noreply.github.com>
2022-02-01 11:58:49 +0100

    Create constant.go

---
+++ Boid/utils/constant.go
@@ -0,0 +1,10 @@
+package utils
+
+const (
+	ScreenWidth              = 1152
+	ScreenHeight             = 648
+	NumBoids                 = 200
+	NumPreda                 = 3
+	WallSeparationPerception = 25.0 //increase it and the wall will looks like a repulsive object
+	NumSpecies               = 3
+)
